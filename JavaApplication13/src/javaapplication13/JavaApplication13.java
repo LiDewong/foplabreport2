38d1fdf40024df9111e6d02f41a7332fd347394d
LiDewong <95974704+LiDewong@users.noreply.github.com>
2021-12-16 01:55:45 +0800

    helo

--- JavaApplication13/src/javaapplication13/JavaApplication13.java
+++ JavaApplication13/src/javaapplication13/JavaApplication13.java
@@ -15,6 +15,7 @@ public class JavaApplication13 {
      */
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("how are you");
     }
     
 }
