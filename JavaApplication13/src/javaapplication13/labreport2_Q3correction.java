93cb9852c32e88bfaaef8fafb5f1437bda7f34b6
Christina Ku Pei San <u2102789@siswa.um.edu.my>
2022-01-01 13:45:42 +0800

    my initial commit

--- JavaApplication13/src/javaapplication13/labreport2_Q3correction.java
+++ JavaApplication13/src/javaapplication13/labreport2_Q3correction.java
@@ -1,6 +1,6 @@
 package javaapplication13;
 
-public class labreport2_Q3ck_short {
+public class labreport2_Q3correction {
     
     public static int divisibleNum(String s, int k) {
         String[] input = s.split("");
@@ -19,13 +19,16 @@ public class labreport2_Q3ck_short {
         int index1 = s.indexOf("?");
         int index2 = s.indexOf("?", index1+1);
         int index3 = s.indexOf("?", index2+1);
-        String s1, s2, s3;
+        String s1;
         
         for (int i = index1 == 0? 1 : 0; i < 10; i++) {
-            s1 = "";
             input[index1] = String.valueOf(i);
-            s1 += input[index1];
             if (count1 == 1) {
+                s1 = "";
+                input[index1] = String.valueOf(i);
+                for (int j = 0; j < input.length; j++) {
+                    s1 += input[j];
+                }
                 System.out.print(s1 + " ");
                 if (Integer.parseInt(s1) % k == 0)
                     count2++;
@@ -34,21 +37,27 @@ public class labreport2_Q3ck_short {
             else {
                 for (int j = 0; j < 10; j++) {
                     input[index2] = String.valueOf(j);
-                    s2 = s1 + input[index2];
                     if (count1 == 2) {
-                        System.out.print(s2 + " ");
-                        if (Integer.parseInt(s2) % k == 0)
+                        s1 = "";
+                        for (int m = 0; m < input.length; m++) {
+                            s1 += input[m];
+                        }
+                        System.out.print(s1 + " ");
+                        if (Integer.parseInt(s1) % k == 0)
                             count2++;
                     }
                     
                     else {
                         for (int m = 0; m < 10; m++) {
                             input[index3] = String.valueOf(m);
-                            s3 = s2 + input[index3];
-                            System.out.print(s3 + " ");
-                            if (Integer.parseInt(s3) % k == 0)
+                            s1 = "";
+                            for (int n = 0; n < input.length; n++) {
+                                s1 += input[n];
+                            }
+                            System.out.print(s1 + " ");
+                            if (Integer.parseInt(s1) % k == 0)
                                 count2++;
-                        }
+                            }
                     }
                 }
             }
